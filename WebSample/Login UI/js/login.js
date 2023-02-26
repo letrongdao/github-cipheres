69932b8ccf0bd2f92700f59fa75e25781ddca743
letrongdao <122431769+letrongdao@users.noreply.github.com>
2023-02-27 01:40:23 +0700

    Delete WebSample/Login UI directory

--- WebSample/Login UI/js/login.js
+++
@@ -1,7 +0,0 @@
-var link = document.getElementsByClassName("createacc");
-link.addEventListener("click", signUp());
-
-function signUp() {
-    x = document.getElementById("signup");
-    x.setAttribute("display", "block");
-}
