23e0f1574d87f99d57c07cb50dcc1be865b9b1c7
letrongdao <122431769+letrongdao@users.noreply.github.com>
2023-02-27 01:41:05 +0700

    Add Login UI

---
+++ Login UI/js/login.js
@@ -0,0 +1,7 @@
+var link = document.getElementsByClassName("createacc");
+link.addEventListener("click", signUp());
+
+function signUp() {
+    x = document.getElementById("signup");
+    x.setAttribute("display", "block");
+}
